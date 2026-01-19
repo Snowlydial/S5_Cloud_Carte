4b94718508c78ecaba8cb698b5e02dd451982850
TSIALONINA <149304848+Tsialone@users.noreply.github.com>
2026-01-19 23:18:12 +0300

    done

--- frontend-mobile/src/init.ts
+++ frontend-mobile/src/init.ts
@@ -1,11 +1,13 @@
 import { CompteSeeder } from "./seeders/CompteSeeder";
 import { ProfilSeeder } from "./seeders/ProfilSeeder";
+import { TypeSignalementSeeder } from "./seeders/TypeSignalementSeeder";
 import { UserSeeder } from "./seeders/UserSeed";
 
 (async () => {
   await UserSeeder.seed();
   await ProfilSeeder.seed();
   await CompteSeeder.seed();
+  await TypeSignalementSeeder.seed();
   console.log("Seeding terminé ✅");
   process.exit();
 })();
